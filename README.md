e0a77ee21bbe7066c4ad2f2efae4b0ba7a4d4239
amark2005 <amarkeshav14@gmail.com>
2026-03-03 13:41:05 +0530

    shelved

---
+++ README.md
@@ -0,0 +1,1 @@
+#Santa Problem(**Shelved**)
--- main.cpp
+++ main.cpp
@@ -12,9 +12,16 @@
 #include<mutex>
 #include<semaphore>
 constexpr int REINDERR_COUNT=9;
+constexpr int ELF_COUNT=10;
+int elfwaiting=0;
+int reindeer_avail=0;
 std::mutex mux;
 std::counting_semaphore<9> santa_wake(0);
 std::counting_semaphore<9> reindeer_wake(0);
+std::counting_semaphore<3> elf_bouncer(3);  
+std::counting_semaphore<9> elf_signal(0);  
+std::counting_semaphore<9> elf_problem(0);  
+std::counting_semaphore<9> elf_done(0);
 
 void randisleep(int minS,int maxS){
   std::random_device dev;
@@ -24,12 +31,31 @@ void randisleep(int minS,int maxS){
 }
 void santaThread(){
   std::cout<<"santa is sleeping"<<std::endl;
+  while(true){
+    santa_wake.acquire();
+    std::cout<<"[santa] All reindeers here!!!! Time to deliver gifts!!!"<<std::endl;
+    for(int i=0;i<REINDERR_COUNT;i++)reindeer_wake.release();
+    randisleep(0,1);
+    std::cout<<"Toys are Delivered!!! Ho HO HOOOOO!!"<<std::endl;
+    std::lock_guard<std::mutex> lock(mux);
+    reindeer_avail=0;
+  }
+
   //TODO: wake up when 9 deer arrive
 }
 void reindeerThread(int id){
-  randisleep(2,10);
-  std::cout<<"Reindeer Number "<<id<<" Has arrived"<<std::endl;
+  randisleep(1,3);
+  {std::lock_guard<std::mutex> lock(mux);
+  reindeer_avail++;
+  std::cout<<"Reindeer "<<id<<" Has arrived. "<<reindeer_avail<<"Available now\n"<<std::endl;
+  if(reindeer_avail==REINDERR_COUNT){
+    std::cout<<"All the Reindeers have arrived...\n Waking up santa!!! "<<std::endl;
+    santa_wake.release();
+  }}
+  reindeer_wake.acquire();
+  std::cout<<"[Reindeer "<<id<<"] Delivering Toysssss!!!!"<<std::endl;
 }
+
 
 int main(){
   std::thread santa(santaThread);
